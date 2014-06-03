1ec90bcf0ccfe0ea0d526a54860afffb8019cfa4
Ryan Kaskel <dev@ryankaskel.com>
2014-06-03 09:24:24 +0100

    "Working" (but with stack overflows)

--- src/Rievaulx/Web.hs
+++ src/Rievaulx/Web.hs
@@ -4,26 +4,33 @@ module Rievaulx.Web
        ( runServer
        ) where
 
-import Network.HTTP.Types (status200, status404)
-import Network.Wai
-import Network.Wai.Handler.Warp (run)
-import Network.Wai.Middleware.Static (staticPolicy, addBase)
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as LT
+import qualified Data.Text.Lazy.Encoding as LE
+import           Network.HTTP.Types (status200, status404)
+import           Network.Wai
+import           Network.Wai.Handler.Warp (run)
+import           Network.Wai.Middleware.Static (staticPolicy, addBase)
+import           Rievaulx (TernaryTree, insertMany, prefixTerms)
+import           Rievaulx.Sources (getRandomizedWords)
 
-complete req = responseLBS status200 [("Content-Type", "text/html")]
-               "Hello, world!"
-
+notFound :: Response
 notFound = responseLBS status404 [("Content-Type", "text/html")]
            "<h1>404 Not Found</h1>"
 
-application :: Application
-application req = return $ case rawPathInfo req of
-  "/complete" -> complete req
-  _ -> notFound
+createApp :: TernaryTree Char -> Application
+createApp tree = application
+  where application req = return $ case pathInfo req of
+          (_:word:[]) -> responseLBS status200 [("Content-Type", "text/html")]
+                         (LE.encodeUtf8 . LT.pack . show . prefixTerms tree $ T.unpack word)
+          _ -> notFound
 
 staticMiddleware :: Middleware
 staticMiddleware = staticPolicy $ addBase "./public"
 
 runServer :: IO ()
 runServer = do
+  words <- getRandomizedWords "/usr/share/dict/web2"
+  let completionCandidates = insertMany words
   putStrLn $ "Rievaulx is starting on http://localhost:3000/ ..."
-  run 3000 $ staticMiddleware $ application
+  run 3000 $ staticMiddleware $ createApp completionCandidates
